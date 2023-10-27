4fe26add72f1407bc77b0911d66f01d3942f840e
Alexandra Acosta <61189229+MarlyAcosta@users.noreply.github.com>
2023-10-27 12:27:27 -0500

    ajustes correcion

--- src/pages/ConfirmOrder.jsx
+++ src/pages/ConfirmOrder.jsx
@@ -10,18 +10,18 @@ export const ConfirmOrder = () => {
     const navigate = useNavigate();
 
     return (
-        <main>
+        <main className='main'>
             <section className="page_success">
                 <p>Hola,
                     <span className="page_data">{nombre}</span>
                 </p>
-                <p>Tu pedido
-                    <span className="page_data">#325235</span>
+                <p>Tu pedido 
+                    <span className="page_data"> #325235 </span>
                     fue realizado sactifactoriamente, Se estara entregando en la siguiente direccion:
-                    <span className="page_data">{direccion}</span>
+                    <span className="page_data"> {direccion} </span>
                 </p>
                 <p>Te estaremos actualizando de como continua el proceso al siguiente correo:
-                    <span className="page_data">{email}</span>
+                    <span className="page_data"> {email} </span>
                 </p>
                 <p>Gracias por confiar en nosotros</p>
                 <a className="page_btn" href="/">Regresar al Home</a>
